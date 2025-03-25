f6f609533a0bd6b2532604297cf973ef6b6328a8
SillyCatto <raiyanmuhtasim@gmail.com>
2025-03-25 23:23:37 +0600

    moved calculateFlightTime to MathUtils class, since it better belongs there

--- src/Flight.java
+++ src/Flight.java
@@ -52,7 +52,7 @@ public class Flight{
         this.toWhichCity = chosenDestinations[1][0];
         this.distanceInMiles = Double.parseDouble(distanceBetweenTheCities[0]);
         this.distanceInKm = Double.parseDouble(distanceBetweenTheCities[1]);
-        this.flightTime = calculateFlightTime(distanceInMiles);
+        this.flightTime = MathUtils.calculateFlightTime(distanceInMiles);
         this.listOfRegisteredCustomersInAFlight = new ArrayList<>();
         this.gate = gate;
     }
@@ -118,34 +118,7 @@ public class Flight{
      * @param distanceBetweenTheCities distance between the cities/airports in miles
      * @return formatted flight time
      */
-    public String calculateFlightTime(double distanceBetweenTheCities) {
-        double groundSpeed = 450;
-        double time = (distanceBetweenTheCities / groundSpeed);
-        String timeInString = String.format("%.4s", time);
-        String[] timeArray = timeInString.replace('.', ':').split(":");
-        int hours = Integer.parseInt(timeArray[0]);
-        int minutes = Integer.parseInt(timeArray[1]);
-        int modulus = minutes % 5;
-        // Changing flight time to make minutes near/divisible to 5.
-        if (modulus < 3) {
-            minutes -= modulus;
-        } else {
-            minutes += 5 - modulus;
-        }
-        if (minutes >= 60) {
-            minutes -= 60;
-            hours++;
-        }
-        if (hours <= 9 && Integer.toString(minutes).length() == 1) {
-            return String.format("0%s:%s0", hours, minutes);
-        } else if (hours <= 9 && Integer.toString(minutes).length() > 1) {
-            return String.format("0%s:%s", hours, minutes);
-        } else if (hours > 9 && Integer.toString(minutes).length() == 1) {
-            return String.format("%s:%s0", hours, minutes);
-        } else {
-            return String.format("%s:%s", hours, minutes);
-        }
-    }
+
 
     /**
      * Creates flight arrival time by adding flight time to flight departure time
--- src/MathUtils.java
+++ src/MathUtils.java
@@ -6,4 +6,33 @@ public class MathUtils {
     public static double radianToDegree(double rad) {
         return (rad * 180.0 / Math.PI);
     }
+
+    public static String calculateFlightTime(double distanceBetweenTheCities) {
+        double groundSpeed = 450;
+        double time = (distanceBetweenTheCities / groundSpeed);
+        String timeInString = String.format("%.4s", time);
+        String[] timeArray = timeInString.replace('.', ':').split(":");
+        int hours = Integer.parseInt(timeArray[0]);
+        int minutes = Integer.parseInt(timeArray[1]);
+        int modulus = minutes % 5;
+        // Changing flight time to make minutes near/divisible to 5.
+        if (modulus < 3) {
+            minutes -= modulus;
+        } else {
+            minutes += 5 - modulus;
+        }
+        if (minutes >= 60) {
+            minutes -= 60;
+            hours++;
+        }
+        if (hours <= 9 && Integer.toString(minutes).length() == 1) {
+            return String.format("0%s:%s0", hours, minutes);
+        } else if (hours <= 9 && Integer.toString(minutes).length() > 1) {
+            return String.format("0%s:%s", hours, minutes);
+        } else if (hours > 9 && Integer.toString(minutes).length() == 1) {
+            return String.format("%s:%s0", hours, minutes);
+        } else {
+            return String.format("%s:%s", hours, minutes);
+        }
+    }
 }
