99a4e1afa8f5ed1070065e901b3ab62db5a71182
SillyCatto <raiyanmuhtasim@gmail.com>
2025-03-25 23:16:29 +0600

    removed broken hierarchy

--- src/RolesAndPermissions.java
+++ src/RolesAndPermissions.java
@@ -1,4 +1,4 @@
-public class RolesAndPermissions extends User {
+public class RolesAndPermissions{
     //        ************************************************************ Behaviours/Methods ************************************************************
 
     /**
@@ -9,9 +9,9 @@ public class RolesAndPermissions extends User {
      */
     public int isPrivilegedUserOrNot(String username, String password) {
         int isFound = -1;
-        for (int i = 0; i < adminCredentials.length; i++) {
-            if (username.equals(adminCredentials[i][0])) {
-                if (password.equals(adminCredentials[i][1])) {
+        for (int i = 0; i < User.adminCredentials.length; i++) {
+            if (username.equals(User.adminCredentials[i][0])) {
+                if (password.equals(User.adminCredentials[i][1])) {
                     isFound = i;
                     break;
                 }
@@ -28,7 +28,7 @@ public class RolesAndPermissions extends User {
      */
     public String isPassengerRegistered(String email, String password) {
         String isFound = "0";
-        for (Customer c : Customer.customerCollection) {
+        for (Customer c : Customer.getCustomersCollection()) {
             if (email.equals(c.getEmail())) {
                 if (password.equals(c.getPassword())) {
                     isFound = "1-" + c.getUserID();
